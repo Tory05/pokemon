4475addeb35e475c53b8e02ce65f106e8c0dc220
Toroczkai Attila <attilatoroczkai@outlook.com>
2022-01-29 15:00:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("pokemon-name");
+      title = document.getElementById("pokemon-title");
+      img = document.getElementById("pokemon-img");
+    
+
+    
+document.getElementById("btn").addEventListener("click", getPokemon);
+
+function getPokemon(){
+
+    var api_url = `https://pokeapi.co/api/v2/pokemon/${input.value}`;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", api_url, true);
+
+    xhr.onload = function(){
+
+        if(xhr.status === 200){
+
+            var pokemon = JSON.parse(xhr.responseText);
+            console.log(pokemon);
+
+            img.setAttribute("src", pokemon.sprites.front_default);
+            title.textContent = pokemon.name;
+    
+
+            input.value = "";
+        }
+    }
+    xhr.send();
+}
