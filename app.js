8804d956d7f70f567547886bbc50ad454003e52a
Toroczkai Attila <attilatoroczkai@outlook.com>
2022-01-29 15:00:39 +0100

    teszt

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const input = document.getElementById("pokemon-name");
-      title = document.getElementById("pokemon-title");
-      img = document.getElementById("pokemon-img");
-    
+title = document.getElementById("pokemon-title");
+img = document.getElementById("pokemon-img");
 
-    
+
 document.getElementById("btn").addEventListener("click", getPokemon);
 
-function getPokemon(){
+function getPokemon() {
 
     var api_url = `https://pokeapi.co/api/v2/pokemon/${input.value}`;
 
@@ -14,16 +13,16 @@ function getPokemon(){
 
     xhr.open("GET", api_url, true);
 
-    xhr.onload = function(){
+    xhr.onload = function() {
 
-        if(xhr.status === 200){
+        if (xhr.status === 200) {
 
             var pokemon = JSON.parse(xhr.responseText);
             console.log(pokemon);
 
             img.setAttribute("src", pokemon.sprites.front_default);
             title.textContent = pokemon.name;
-    
+
 
             input.value = "";
         }
